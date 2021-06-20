68fd80caa134d4c72b32acd5827b758736ae4523
Ashley Kilgour <kilgourkilgour@hotmail.com>
2021-06-20 22:52:29 +0100

    Added IsPINConsecutiveDescendingNumbers and its test

---
+++ Pins.Application.Test/Helper/IsPINConsecutiveDescendingNumbersTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Pins.Application.Helper;
+
+namespace Pins.Application.Test.Helper
+{
+    public class IsPINConsecutiveDescendingNumbersTest
+    {
+        [TestCase("9876")]
+        [TestCase("8765")]
+        [TestCase("4321")]
+        [TestCase("3210")]
+        public void Resolve_ConsecutiveDescendingNumbers(string pin)
+        {
+            //arrange
+
+            //act
+            var value = IsPINConsecutiveDescendingNumbers.Resolve(pin);
+            //assert
+            Assert.IsTrue(value);
+        }
+
+        [TestCase("1234")]
+        [TestCase("7890")]
+        [TestCase("1369")]
+        [TestCase("9753")]
+        public void Resolve_NotConsecutiveDescendingNumbers(string pin)
+        {
+            //arrange
+
+            //act
+            var value = IsPINConsecutiveDescendingNumbers.Resolve(pin);
+            //assert
+            Assert.IsFalse(value);
+        }
+    }
+}
---
+++ Pins.Application/Helper/IsPINConsecutiveDescendingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Pins.Application.Helper
+{
+    public static class IsPINConsecutiveDescendingNumbers
+    {
+        public static bool Resolve(string pin)
+        {
+            var numbers = pin.ToList();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] - 1 != numbers[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
