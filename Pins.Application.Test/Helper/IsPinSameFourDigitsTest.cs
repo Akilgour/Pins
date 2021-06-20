1d3134eb79ddfa1c43171d770a58a8ce9e753a68
Ashley Kilgour <kilgourkilgour@hotmail.com>
2021-06-20 23:02:39 +0100

    Added check to see if all 4 digits in a pin are the same

---
+++ Pins.Application.Test/Helper/IsPinSameFourDigitsTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Pins.Application.Helper;
+
+namespace Pins.Application.Test.Helper
+{
+    public class IsPinSameFourDigitsTest
+    {
+        [TestCase("0000")]
+        [TestCase("1111")]
+        [TestCase("5555")]
+        [TestCase("9999")]
+        public void Resolve_SameFourDigits(string pin)
+        {
+            //arrange
+
+            //act
+            var value = IsPinSameFourDigits.Resolve(pin);
+            //assert
+            Assert.True(value);
+        }
+
+        [TestCase("0001")]
+        [TestCase("1000")]
+        [TestCase("1234")]
+        [TestCase("1110")]
+        [TestCase("1010")]
+        [TestCase("1357")]
+        public void Resolve_NotSameFourDigits(string pin)
+        {
+            //arrange
+
+            //act
+            var value = IsPinSameFourDigits.Resolve(pin);
+            //assert
+            Assert.IsFalse(value);
+        }
+    }
+}
---
+++ Pins.Application/Helper/IsPinSameFourDigits.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Pins.Application.Helper
+{
+    public class IsPinSameFourDigits
+    {
+        public static bool Resolve(string pin)
+        {
+            var numbers = pin.ToList();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] != numbers[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
